f3f2f685abd1a3aa18f3873c562f59069907e752
Reilly <rwebster_33@hotmail.com>
2021-05-24 19:52:13 -0300

    Added script to choose random ore

---
+++ MinerInconvenience/OreVein.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+var gold_vein_texture = preload("res://images/GoldVein.png")
+var pyrite_vein_texture = preload("res://images/PyriteVein.png")
+
+onready var vein_sprite = get_node("Sprite")
+
+func _ready() -> void:
+	set_sprite()
+	
+func set_sprite():
+	match get_random_sprite():
+		1: vein_sprite.set_texture(gold_vein_texture)
+		2: vein_sprite.set_texture(pyrite_vein_texture)
+
+func get_random_sprite():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	return rng.randi_range(1, 2)
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		get_tree().reload_current_scene()
