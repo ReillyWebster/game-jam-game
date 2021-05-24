e7bcfb92aebd8f5db644a54380daa10c0645e6e1
Reilly <rwebster_33@hotmail.com>
2021-05-24 20:02:49 -0300

    Added a veinType to use type differentiation

--- MinerInconvenience/OreVein.gd
+++ MinerInconvenience/OreVein.gd
@@ -1,17 +1,26 @@
 extends Area2D
 
+onready var vein_sprite = get_node("Sprite")
+
 var gold_vein_texture = preload("res://images/GoldVein.png")
 var pyrite_vein_texture = preload("res://images/PyriteVein.png")
 
-onready var vein_sprite = get_node("Sprite")
+var veinType
+
+enum VeinType {Gold, Pyrite}
 
 func _ready() -> void:
 	set_sprite()
 	
 func set_sprite():
 	match get_random_sprite():
-		1: vein_sprite.set_texture(gold_vein_texture)
-		2: vein_sprite.set_texture(pyrite_vein_texture)
+		1: 
+			vein_sprite.set_texture(gold_vein_texture)
+			veinType = VeinType.Gold
+			
+		2: 
+			vein_sprite.set_texture(pyrite_vein_texture)
+			veinType = VeinType.Pyrite
 
 func get_random_sprite():
 	var rng = RandomNumberGenerator.new()
