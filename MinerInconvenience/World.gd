b60859c5082bd57e9a3926df026a5857491c0465
Marc <48297737+BrokenTwin@users.noreply.github.com>
2021-05-24 18:39:34 -0300

    Initial Project Start
    Created the following scenes:
    World
    Player
    OreVein
    Exit
    
    And created a file to track the source of the various resources we use for crediting.

---
+++ MinerInconvenience/World.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+var borders = Rect2(1, 1, 38, 21)
+
+onready var tileMap = $TileMap
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	generate_level()
+
+func generate_level():
+	var walker = Walker.new(Vector2(19, 10), borders)
+	var map = walker.walk(500)
+	walker.queue_free()
+	
+	for location in map:
+		tileMap.set_cellv(location, -1)
+	tileMap.update_bitmask_region(borders.position, borders.end)
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		get_tree().reload_current_scene()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
