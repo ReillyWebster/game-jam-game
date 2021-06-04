6641f2f7ced5e57d8d427335a68ba028445bd807
Reilly <rwebster_33@hotmail.com>
2021-06-04 08:03:14 -0300

    Updated start menu to use main menu script

--- MinerInconvenience/scenes/main_menu/StartMenu.gd
+++ MinerInconvenience/scenes/main_menu/StartMenu.gd
@@ -1,21 +1,5 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$MarginContainer/VBoxContainer/HBoxContainer/LblTotalGold.text = "You died with " + str(Global.current_gold) + " gold!"
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Button_pressed():
 	Global.current_gold = 0
 	Global.current_pyrite = 0
